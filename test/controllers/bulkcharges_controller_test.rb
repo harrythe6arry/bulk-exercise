f97dc2f9aee9812b386b387928e40f87d8537697
harrythe6arry <voravit.sri@student.mahidol.edu>
2025-07-29 15:31:27 +0700

    fix: remove secret key

--- test/controllers/bulkcharges_controller_test.rb
+++ test/controllers/bulkcharges_controller_test.rb
@@ -4,4 +4,10 @@ class BulkchargesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+  test "should get index" do
+    get bulkcharges_url
+    assert_response :success
+    assert_select "h1", "Bulk Charges total CSV files "
+  end
 end
